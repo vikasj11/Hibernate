5109282b93948787f1a4ddaaa183c845ccd06873
Vikas <Vikas@DESKTOP-FQQG9BE>
2025-10-28 15:51:29 +0530

    initial commit

---
+++ HibernateProject/src/main/java/com/vikas/HibernateProject/App.java
@@ -0,0 +1,28 @@
+package com.vikas.HibernateProject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory factory = cfg.buildSessionFactory();
+        System.out.println(factory);
+        Teacher t = new Teacher(16,"Manoj","Reactjs");
+        Session ss = factory.openSession();
+        Transaction tran = ss.beginTransaction();
+        ss.save(t);
+        tran.commit();
+    
+    }
+    
+}
