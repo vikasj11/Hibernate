89ad1826e3faa04ba1feaeb2dc4892a0a642112b
vikasj11 <120361319+vikasj11@users.noreply.github.com>
2026-07-08 12:36:34 +0530

    Updated fixes #1

--- HibernateProject/src/main/java/com/vikas/HibernateProject/App.java
+++ HibernateProject/src/main/java/com/vikas/HibernateProject/App.java
@@ -5,10 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
